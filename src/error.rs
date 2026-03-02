15b9d1ab8cb32d04c338ebe8af9911f6d4974c6f
Chris Reynolds <chris@reynolds.guru>
2026-03-01 18:03:55 -0600

    Checkpoint.

--- src/error.rs
+++
@@ -1,8 +0,0 @@
-/// Crate-level error type (only used by `NibbleReader` in tests).
-#[cfg(test)]
-#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
-pub(crate) enum Error {
-    /// Attempted to read past the end of the input.
-    #[error("unexpected end of input")]
-    UnexpectedEnd,
-}
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,5 @@
 
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
-mod adler32;
-mod bleb8;
 pub mod decoder;
 pub mod encoder;
-mod error;
-mod nibble;
-
-/// Magic bytes: `LZR`.
-pub(crate) const MAGIC: [u8; 3] = [0x4C, 0x5A, 0x52];
-
-/// Format version byte.
-pub(crate) const VERSION: u8 = 0x00;
-
-/// Sliding window size (64 KiB).
-pub(crate) const WINDOW_SIZE: usize = 65_536;
